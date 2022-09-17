32e13a494819686a3ac94f6b28245aa0a2a4a7ee
Thombuhl <thomas.m.buhl@gmail.com>
2022-09-17 10:50:03 -0400

    commit

---
+++ server/routes/api/Middleware.js/middleware.js
@@ -0,0 +1,14 @@
+const { User } = require("../../../db/models/User");
+
+const isLoggedIn = async (req, res, next) => {
+  try {
+    req.user = await User.findByToken(req.headers.authorization);
+    next();
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports = {
+  isLoggedIn,
+};
