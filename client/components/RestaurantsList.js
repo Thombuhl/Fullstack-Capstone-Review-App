098586c520fffd79cc8f73d5380dd32e116efba6
Thombuhl <thomas.m.buhl@gmail.com>
2022-09-22 19:50:33 -0400

    added facet search params to UI

--- client/components/RestaurantsList.js
+++ client/components/RestaurantsList.js
@@ -1,54 +1,182 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import RestaurantItem from './RestaurantItem';
+import {
+    ResturantsContainer,
+    ResturantFilter,
+    Select,
+    Option,
+} from '../styledComponents/RestaurantList';
 
 import '../styles/RestaurantsList.css';
 
 const RestaurantsList = () => {
+    //No "ownProps" with useSelector hook. Set filter object using React useState hook.
     const { restaurants } = useSelector((state) => state);
 
+    //Store the user's filter selection.
     const [filter, setFilter] = useState({});
 
-    const handleFilters = (event) => {
-        const value = event.target.value;
+    //Display the restaurants that have properties matching the filter.
+    const [filteredRestaurants, setfilteredRestaurants] = useState({});
+
+    const stashFilteredAttributes = (evt) => {
+        const value = evt.target.value;
         setFilter({
             ...filter,
-            [event.target.name]: value,
+            [evt.target.name]: value,
         });
     };
 
-    const deliveryMap = restaurants
-        .filter(
-            (restaurant) =>
-                !filter.hasDelivery ||
-                restaurant.hasDelivery === filter.hasDelivery
-        )
-        .reduce((acc, restaurant) => {
-            const hasDelivery = restaurant.hasDelivery;
-            acc[hasDelivery] = acc[hasDelivery] || {
-                id: restaurant.id,
-                hasDelivery,
-                count: 0,
-            };
-            acc[key].count++;
-            return acc;
-        }, {});
+    // Array of 100 restaurants
+    const restaurantsArr = restaurants;
+    // console.log(restaurants);
+
+    //All Categories.
+    const categoriess = restaurantsArr.reduce((acc, restaurant) => {
+        const category = restaurant.category;
+        acc[category] = acc[category] || {
+            id: restaurant.id,
+            category,
+            count: 0,
+        };
+        acc[category].count++;
+        return acc;
+    }, {});
+    const categoryNames = Object.values(categoriess);
+
+    const prices = restaurantsArr.reduce((acc, restaurant) => {
+        const price = restaurant.price;
+        acc[price] = acc[price] || {
+            id: restaurant.id,
+            price,
+            count: 0,
+        };
+        acc[price].count++;
+        return acc;
+    }, {});
+    const bougie = Object.values(prices);
+
+    const ratings = restaurantsArr.reduce((acc, restaurant) => {
+        const rating = restaurant.rating;
+        acc[rating] = acc[rating] || {
+            id: restaurant.id,
+            rating,
+            count: 0,
+        };
+        acc[rating].count++;
+        return acc;
+    }, {});
+    const reviews = Object.values(ratings);
+
+    //Restaurant delivery option
+    const deliveries = restaurantsArr.reduce((acc, restaurant) => {
+        const delivery = restaurant.hasDelivery;
+        acc[delivery] = acc[delivery] || {
+            id: restaurant.id,
+            delivery,
+            count: 0,
+        };
+        acc[delivery].count++;
+        return acc;
+    }, {});
+    const deliveryBool = Object.values(deliveries);
+
+    //Restaurant pickup option
+    const pickups = restaurantsArr.reduce((acc, restaurant) => {
+        const pickup = restaurant.hasPickup;
+        acc[pickup] = acc[pickup] || {
+            id: restaurant.id,
+            pickup,
+            count: 0,
+        };
+        acc[pickup].count++;
+        return acc;
+    }, {});
+    const pickupBool = Object.values(pickups);
+
+    const reservations = restaurantsArr.reduce((acc, restaurant) => {
+        const reservation = restaurant.hasReservation;
+        acc[reservation] = acc[reservation] || {
+            id: restaurant.id,
+            reservation,
+            count: 0,
+        };
+        acc[reservation].count++;
+        return acc;
+    }, {});
+    const reservationBool = Object.values(reservations);
 
     return (
-        <>
+        <ResturantsContainer>
+            <ResturantFilter>
+                <Select name="category" onChange={stashFilteredAttributes}>
+                    <Option>Categories</Option>
+                    {categoryNames.map((category) => {
+                        return (
+                            <Option key={category.id}>
+                                {category.category}
+                            </Option>
+                        );
+                    })}
+                </Select>
+                <Select name="ratings" onChange={stashFilteredAttributes}>
+                    <Option>Rating</Option>
+                    {reviews.map((rate) => {
+                        return <Option key={rate.id}>{rate.rating}</Option>;
+                    })}
+                </Select>
+                <Select name="prices" onChange={stashFilteredAttributes}>
+                    <Option>Price</Option>
+                    {bougie.map((price) => {
+                        if (!price.price) {
+                            return;
+                        } else {
+                            return (
+                                <Option key={price.id}>{price.price}</Option>
+                            );
+                        }
+                    })}
+                </Select>
+                <Select name="delivery" onChange={stashFilteredAttributes}>
+                    <Option>Delivery</Option>
+                    {deliveryBool.map((element) => {
+                        return (
+                            <Option key={element.id}>
+                                {String(element.delivery).toUpperCase()}
+                            </Option>
+                        );
+                    })}
+                </Select>
+                <Select name="pickup" onChange={stashFilteredAttributes}>
+                    <Option>Pickup</Option>
+                    {pickupBool.map((element) => {
+                        return (
+                            <Option key={element.id}>
+                                {String(element.pickup).toUpperCase()}
+                            </Option>
+                        );
+                    })}
+                </Select>
+                <Select name="reservation" onChange={stashFilteredAttributes}>
+                    <Option>Reservations</Option>
+                    {reservationBool.map((element) => {
+                        return (
+                            <Option key={element.id}>
+                                {String(element.reservation).toUpperCase()}
+                            </Option>
+                        );
+                    })}
+                </Select>
+            </ResturantFilter>
+
             <div className="RestaurantsList">
                 {restaurants.map((res) => (
-                    <RestaurantItem restaurant={res} key={res.id} />
+                    <RestaurantItem restaurant={res} />
                 ))}
             </div>
-            <select name="hasDelivery" onChange={handleFilters}>
-                <option>Delivery</option>
-                {deliveryMap.map((item) => {
-                    return <option key={item.id}>{item.hasDelivery}</option>;
-                })}
-            </select>
-        </>
+        </ResturantsContainer>
     );
 };
 
---
+++ client/styledComponents/RestaurantList.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ResturantsContainer = styled.div`
+    margin: 2rem;
+`;
+
+export const ResturantFilter = styled.div`
+    margin: 1rem;
+`;
+
+export const Select = styled.select`
+    margin: 1rem;
+`;
+
+export const Option = styled.option`
+    margin: 1rem;
+`;
