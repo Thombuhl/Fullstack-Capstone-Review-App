044b7f87af26c1890252fea640c6b6bf1afa9465
Kieran B <kieranbru.web@gmail.com>
2022-09-24 12:47:52 -0400

    Fixed rating refresh

--- client/pages/Restaurant.js
+++ client/pages/Restaurant.js
@@ -1,39 +1,18 @@
 import React, { useReducer, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import RestaurantsList from '../components/RestaurantsList';
 
 import '../styles/OneRestaurant.css';
 import { Link } from 'react-router-dom';
-import Image from 'react-bootstrap/Image';
-import {
-    SingleRestContainer,
-    // SingleRestContainer,
-    // SliderValueText,
-} from '../styledComponents/SingleRestaurant';
 import CreateRatingForm from '../components/CreateRatingForm';
-import { fetchRatings, fetchRestaurants } from '../store';
 
 /**
  * COMPONENT
  */
 const Restaurant = (props) => {
-    const allRatings = useSelector((state) => state.ratings);
+    const { ratings } = useSelector((state) => state);
+    const { restaurants } = useSelector((state) => state);
     const restaurantId = props.match.params.id || {};
-    console.log(props);
-    const dispatch = useDispatch();
 
-    const [restaurants, setRestaurants] = useState([]);
-    const [ratings, setRatings] = useState(allRatings);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const restaurants = (await dispatch(fetchRestaurants())).payload;
-            setRestaurants(restaurants);
-            const ratings = (await dispatch(fetchRatings())).payload;
-            setRatings(ratings);
-        };
-        fetchData();
-    }, []);
     const restaurant =
         restaurants?.find((restaurant) => restaurantId * 1 === restaurant.id) ||
         {};
