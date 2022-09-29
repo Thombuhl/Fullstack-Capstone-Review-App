77260703db89068b846bec326f8c1b3a92736c34
Kieran B <kieranbru.web@gmail.com>
2022-09-29 19:45:17 -0400

    Update restaurant items to have actual info

--- client/components/RestaurantItem.js
+++ client/components/RestaurantItem.js
@@ -1,56 +1,60 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/RestaurantItem.css';
 import { Link } from 'react-router-dom';
+import { fetchResReviews } from '../apiCalls';
 
 const RestaurantsItem = ({ restaurant }) => {
+    const [review, setReview] = useState('');
     useEffect(() => {
-        // const marker = new google.maps.Marker({
-        //     position: {
-        //         lat: restaurant.latitude,
-        //         lng: restaurant.longitude,
-        //     },
-        //     title: restaurant.name,
-        //     animation: google.maps.Animation.DROP,
-        // });
-        // marker.setMap(googleMap);
+        const fetchAReview = async () => {
+            const resReviews = await fetchResReviews(restaurant.id);
+            setReview(
+                `${resReviews[0].user.username} - ${resReviews[0].comment}`
+            );
+        };
+        fetchAReview();
     }, []);
     return (
         <div className="list-group-item list-group-item-action d-flex gap-3 py-3 my-1 card shadow-sm col-md-6">
-        <div className="row">
-            <div className="thumb-post col-auto">
-            <img src={restaurant.imgUrl}  className="img-thumbnail"/>
+            <div className="row">
+                <div className="thumb-post col-auto">
+                    <img src={restaurant.imgUrl} className="img-thumbnail" />
+                </div>
+                <div className="RestaurantItem-content card-block col">
+                    <p className="RestaurantItem-name">
+                        <Link to={`/restaurants/${restaurant.id}`}>
+                            <p className="card-title h5"> {restaurant.name}</p>
+                        </Link>
+                    </p>
+                    <div className="RestaurantItem-rating row">
+                        <div className="col-auto">
+                            <strong>Rating Scores</strong>
+                        </div>
+                        <div className="col">{restaurant.rating}</div>
+                    </div>
+                    <div className="RestaurantItem-preference row">
+                        <div className="col-auto">
+                            <strong className="text-pur">
+                                Your Preference Match
+                            </strong>
+                        </div>
+                        <div className="col">96%</div>
+                    </div>
+                    <div className="RestaurantItem-first-comment">
+                        {review ? review : 'No Reviews.'}
+                    </div>
+                    <div className="d-flex flex-row-reverse ">
+                        <Link to={`/restaurants/${restaurant.id}`}>
+                            <button
+                                type="button"
+                                class="btn btn-sm btn-outline-light"
+                            >
+                                View
+                            </button>{' '}
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <div className="RestaurantItem-content card-block col">
-                <p className="RestaurantItem-name">
-                    <Link to={`/restaurants/${restaurant.id}`}>
-                       <p className="card-title h5"> {restaurant.name}</p>
-                    </Link>
-                </p>
-                <div className="RestaurantItem-rating row">
-                 <div className="col-auto">
-                <strong>Rating Scores</strong>
-                </div>
-                <div className="col">
-                10
-                </div>
-                </div>
-                <div className="RestaurantItem-preference row">
-                 <div className="col-auto">
-                <strong className="text-pur">Prefenrence Fits</strong>
-                </div>
-                <div className="col">
-                96%
-                </div>
-                </div>
-                <div className="RestaurantItem-first-comment">
-                    {' '}
-                    This place rocks
-                </div>
-                <div className="d-flex flex-row-reverse ">
-                  <Link to={`/restaurants/${restaurant.id}`}><button type="button" class="btn btn-sm btn-outline-light">View</button> </Link>
-                </div>
-            </div>
-        </div>
         </div>
     );
 };
