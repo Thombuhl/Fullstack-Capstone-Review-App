be7ac2e39d1a29e08b582613c0f9df1b8084e06f
Kieran B <kieranbru.web@gmail.com>
2022-09-28 20:37:13 -0400

    When updating zip, now restaurants filter

--- client/components/AddressPicker.js
+++ client/components/AddressPicker.js
@@ -1,13 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { setMapPosition } from '../store';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMapPosition, filterRestaurants } from '../store';
+import { fetchAllRes } from '../apiCalls';
+
+const getNearbyRes = (restaurantArray, center, miles = 15) => {
+    const lat = miles / 69,
+        lng = miles / 54.6;
+    return restaurantArray.filter((res) => {
+        return (
+            Number.parseFloat(res.latitude) <= center.lat + lat &&
+            Number.parseFloat(res.latitude) >= center.lat - lat &&
+            Number.parseFloat(res.longitude) <= center.lng + lng &&
+            Number.parseInt(res.longitude) >= center.lng - lng
+        );
+    });
+};
 
 const AddressPicker = () => {
     const [geocoder, setGeocoder] = useState();
     const dispatch = useDispatch();
+    const history = useHistory();
+    const { mapPosition } = useSelector((state) => state.maps);
     useEffect(() => {
         setGeocoder(new window.google.maps.Geocoder());
     }, []);
+    useEffect(() => {
+        const fetchRes = async () => {
+            let res = await fetchAllRes();
+            dispatch(filterRestaurants(getNearbyRes(res, mapPosition, 5)));
+        };
+        fetchRes();
+    }, [mapPosition]);
     const handleSubmit = (ev) => {
         ev.preventDefault();
         geocoder.geocode(
@@ -21,6 +45,7 @@ const AddressPicker = () => {
                             lng: results[0].geometry.location.lng(),
                         })
                     );
+                    history.push('/home/1');
                     window.scrollTo({ top: 0, behavior: 'smooth' });
                 } else {
                     console.log('There was an error' + status);
