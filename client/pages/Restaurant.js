95125f1a1dbd102674596b73d1f4653130b153b8
chloe.by.chen@gmail.com <chloe.by.chen@gmail.com>
2022-10-01 01:47:27 +0000

    trying to style single restruant

--- client/pages/Restaurant.js
+++ client/pages/Restaurant.js
@@ -1,6 +1,6 @@
 import React, { useReducer, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
 import {
@@ -43,7 +43,10 @@ const Restaurant = (props) => {
     const favPref = bestFeature(ratingsForRestaurant);
 
     return (
+    
         <div className="one-restaurant container">
+           <Button className="backbutton" variant="outline-light" href="/home/1"> Go Back</Button>
+            <div className="row py-3">
             <Card className="bg-dark text-white">
                 <Card.Img
                     src={restaurant.imgUrl}
@@ -56,7 +59,7 @@ const Restaurant = (props) => {
                 />
                 <Card.ImgOverlay>
                     <Card.Header>
-                        <Link to="/home/1">Go Back</Link>
+                
                     </Card.Header>
                     <Card.Title>{restaurant.name}</Card.Title>
                     {regScore && (
@@ -77,9 +80,15 @@ const Restaurant = (props) => {
                     <Card.Text>{restaurant.description}</Card.Text>
                     <Card.Text>{restaurant.fullAddress}</Card.Text>
                 </Card.ImgOverlay>
-            </Card>
-            <CreateRatingForm restaurantId={restaurantId} />
-            <ShowRatingsForRestaurant restaurantId={restaurantId} />
+                </Card>
+            </div>
+            <div className="row">
+                <CreateRatingForm restaurantId={restaurantId} />
+            </div>
+            <div className="row">
+                <ShowRatingsForRestaurant restaurantId={restaurantId} />
+            </div>
+
         </div>
     );
 };
