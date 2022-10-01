7f117a4b6991f4db5cfccd94eb39bcb22b49b56b
Thombuhl <thomas.m.buhl@gmail.com>
2022-09-30 22:23:25 -0400

    added slider classname

--- client/components/CreateRatingForm.js
+++ client/components/CreateRatingForm.js
@@ -47,67 +47,75 @@ const CreateRatingForm = (props) => {
 
     return (
         <div className="card my-3 py-3 text-left">
-        <Form onSubmit={handleSubmit}>
-    <div class="row g-1 align-items-center justify-content-left">
-        <div class='col-auto'>
-            <strong className="card-title" style={{color:"#454343"}} >Start a Review</strong>
-        </div>
-        <div className="container col-auto w-50">
-            <Form.Select
-                name="preferenceId"
-                aria-label="Default select example"
-                onChange={onChange}
-            >
-                <option value="">Pick Your Review Catagory</option>
-                {allPref?.map((preference) => (
-                    <option key={preference.id} value={preference.id}>
-                        {preference.name}
-                    </option>
-                ))}
-            </Form.Select>
-        </div>
-    </div>
-    <div className="row g-1 align-items-center">
-        <div className="col-auto">          
-             <strong className='' style={{color:"#454343"}}>I will give {score}</strong>
-         </div>
-        <div className="col-auto w-25">
-            <Form.Range
-                min="1"
-                max="5"
-                step="1"
-                value={score}
-                name="score"
-                onChange={onChange}
-            />
-            
-        </div>
-    </div>
-    <div className="row g-1 align-items-end">
-        <div className="col-auto container ">
-            <Form.Group className="w-100">
-
-                <Form.Control
-                    as="textarea"
-                    value={comment}
-                    name="comment"
-                    placeholder="Type Your Comment"
-                    onChange={onChange}
-                 
-                />
-            </Form.Group>
-        </div>
-        <div className="col-auto align-bottom">
-            <Button
-                disabled={!score || !comment || !preferenceId}
-                // variant="primary"
-                type="submit"
-            >
-                Submit
-            </Button>
-        </div>
-    </div>
-        </Form>
+            <Form onSubmit={handleSubmit}>
+                <div class="row g-1 align-items-center justify-content-left">
+                    <div class="col-auto">
+                        <strong
+                            className="card-title"
+                            style={{ color: '#454343' }}
+                        >
+                            Start a Review
+                        </strong>
+                    </div>
+                    <div className="container col-auto w-50">
+                        <Form.Select
+                            name="preferenceId"
+                            aria-label="Default select example"
+                            onChange={onChange}
+                        >
+                            <option value="">Pick Your Review Catagory</option>
+                            {allPref?.map((preference) => (
+                                <option
+                                    key={preference.id}
+                                    value={preference.id}
+                                >
+                                    {preference.name}
+                                </option>
+                            ))}
+                        </Form.Select>
+                    </div>
+                </div>
+                <div className="row g-1 align-items-center">
+                    <div className="col-auto">
+                        <strong className="" style={{ color: '#454343' }}>
+                            I will give {score}
+                        </strong>
+                    </div>
+                    <div className="col-auto w-25">
+                        <Form.Range
+                            min="1"
+                            max="5"
+                            step="1"
+                            value={score}
+                            name="score"
+                            onChange={onChange}
+                            className="slider"
+                        />
+                    </div>
+                </div>
+                <div className="row g-1 align-items-end">
+                    <div className="col-auto container ">
+                        <Form.Group className="w-100">
+                            <Form.Control
+                                as="textarea"
+                                value={comment}
+                                name="comment"
+                                placeholder="Type Your Comment"
+                                onChange={onChange}
+                            />
+                        </Form.Group>
+                    </div>
+                    <div className="col-auto align-bottom">
+                        <Button
+                            disabled={!score || !comment || !preferenceId}
+                            // variant="primary"
+                            type="submit"
+                        >
+                            Submit
+                        </Button>
+                    </div>
+                </div>
+            </Form>
         </div>
     );
 };
